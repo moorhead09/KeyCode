3f9ff2b251864a2a88a80141e16dccddc1b91f65
Matthew <matthewmoorhead@yahoo.com>
2016-10-05 10:06:00 -0400

    Update keyPad.js

--- keyPad.js
+++ keyPad.js
@@ -90,7 +90,7 @@ function loadPW(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            consol.log("Returned: " + this.responseText);
+            console.log("Returned: " + this.responseText);
         }
     };
     xhttp.open("GET", "stuff.txt", true);
